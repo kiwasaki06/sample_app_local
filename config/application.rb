ee3659a7a2654bff3f4e252a6606bc67e4d8c95a
kiwasaki06 <jamjamjamjam666@gmail.com>
2019-06-18 20:52:45 +0900

    fix users2

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,9 +13,11 @@ module SampleApp
     config.load_defaults 5.1
     config.i18n.default_locale = :en
     config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.initialize_on_precompile = false
   end
 end
