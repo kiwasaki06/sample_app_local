84d951ba37e41f1afb02a5c9fbb356288621bdaf
kiwasaki06 <jamjamjamjam666@gmail.com>
2019-06-19 09:54:08 +0900

    fix users3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'https://powerful-ridge-27507.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
